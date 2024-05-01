c1722f75a0f0e4494afe8ea054af33eb5b4e278b
Victor Ask Justesen <2234-au704346@users.noreply.gitlab.au.dk>
2024-05-01 16:19:53 +0200

    init - started working on pseudopolynomial knapsack algo

---
+++ algo1.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def knapsack1(weights, values, n, W):
+    output = np.zeros((n + 1, W + 1))
+    for i in range(1, n + 1):
+        for w in range(1, W + 1):
+            if weights[i - 1] <= w:
+                output[i, w] = max(output[i - 1, w], output[i - 1, w - weights[i - 1]] + values[i - 1])
+            else:
+                output[i, w] = output[i - 1, w]
+    return output[n, W]
+
+def knapsack2(weights, values, n, V):
+    output = np.zeros((n + 1, V + 1))
+    for i in range(1, n + 1):
+        for v in range(1, V + 1):
+            
+
+print(knapsack1([1, 2, 3], [10, 15, 40], 3, 6))
