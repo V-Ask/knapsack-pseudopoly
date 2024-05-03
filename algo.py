73078e0e61835cf98b4f1492e3cf2a819f426944
Victor Ask Justesen <2234-au704346@users.noreply.gitlab.au.dk>
2024-05-03 14:20:10 +0200

    implemented pseudopolynomial knapsack solver by value and by weight

--- algo.py
+++ algo.py
@@ -11,11 +11,18 @@ def knapsack1(weights, values, n, W):
     return output[n, W]
 
 def knapsack2(weights, values, n, V):
-    output = np.zeros((n + 1, V + 1))
+    output = np.full((n+1, V+1), 0.0)
+    output[0][1:] = np.infty
     for i in range(1, n + 1):
         for v in range(1, V + 1):
-            #todo
-            return 0
+            if values[i - 1] <= v:
+                output[i, v] = min(output[i - 1, v], output[i - 1, v - values[i - 1]] + weights[i - 1])
+            else:
+                output[i, v] = output[i - 1, v]
+    return output[n, V]
 
+capacity = 8
+solution1 = knapsack1([1, 3, 5, 7], [2, 4, 7, 10], 4, capacity)
+solution2 = knapsack2([1, 3, 5, 7], [2, 4, 7, 10], 4, int(solution1))
 
-print(knapsack1([1, 2, 3], [10, 15, 40], 3, 6))
+print(solution1, solution2)
